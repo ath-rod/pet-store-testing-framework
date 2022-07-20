bf921d092977eaeea8aa8257f9ef3095a2c31e75
Athziri Rodríguez <athziri.rod@gmail.com>
2022-07-20 12:51:48 -0500

    Added base wrapper with headers and base uri in config file

---
+++ resources/helpers/base_wrapper.py
@@ -0,0 +1,6 @@
+class BaseWrapper:
+    def __init__(self):
+        self.headers = {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        }
--- tests/test_wrapper.py
+++ tests/test_wrapper.py
@@ -1,19 +1,24 @@
+import pytest
+
 from core.api_core import APIRequest
 from assertpy import assert_that
 
 request = APIRequest()
 
 
+@pytest.mark.apicore
 def test_non_existing_pet():
     response = request.get('http://petstore.swagger.io/v2/pet/0')
     assert_that(response.status_code).is_equal_to(404)
 
 
+@pytest.mark.apicore
 def test_existing_pet():
-    response = request.get('http://petstore.swagger.io/v2/pet/55')
+    response = request.get('http://petstore.swagger.io/v2/pet/1000')
     assert_that(response.status_code).is_equal_to(200)
 
 
+@pytest.mark.apicore
 def test_connection_error():
     response = request.get('http://petstre.swagger.io/v2/pet/1234')
     assert_that(response.text).is_equal_to('ConnectionError')
