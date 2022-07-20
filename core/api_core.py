22c3d6a34f87676d4774bb7b44a2d9eb69ad7a0d
Athziri Rodríguez <athziri.rod@gmail.com>
2022-07-20 12:34:12 -0500

    Added api core with get, post, and delete. Added ConnectionError handling and hardcoded tests for it and get

---
+++ core/api_core.py
@@ -0,0 +1,67 @@
+from dataclasses import dataclass
+from requests import ConnectionError
+
+import requests
+
+from resources.utils.custom_strings import error_name
+
+
+@dataclass
+class Response:
+    status_code: int
+    text: str
+    as_dict: object
+    headers: dict
+
+
+class APIRequest:
+    def get(self, url):
+        try:
+            response = requests.get(url)
+            return self.__get_successful_response(response)
+
+        except ConnectionError as error:
+            return self.__get_unsuccessful_response(error)
+
+    def post(self, url, payload, headers):
+        try:
+            response = requests.post(url, data=payload, headers=headers)
+            return self.__get_successful_response(response)
+
+        except ConnectionError as error:
+            return self.__get_unsuccessful_response(error)
+
+    def delete(self, url):
+        try:
+            response = requests.delete(url)
+            return self.__get_successful_response(response)
+
+        except ConnectionError as error:
+            return self.__get_unsuccessful_response(error)
+
+    @staticmethod
+    def __get_successful_response(response):
+        status_code = response.status_code
+        text = response.text
+
+        try:
+            as_dict = response.json()
+        except Exception:
+            as_dict = {}
+
+        headers = response.headers
+
+        return Response(
+            status_code, text, as_dict, headers
+        )
+
+    @staticmethod
+    def __get_unsuccessful_response(error):
+        status_code = 0
+        text = error_name(error)
+        as_dict = {'error type': error_name(error)}
+        headers = {}
+
+        return Response(
+            status_code, text, as_dict, headers
+        )
---
+++ resources/utils/custom_strings.py
@@ -0,0 +1,4 @@
+def error_name(error):
+    name_start = str(type(error)).rfind('.') + 1
+    name = str(type(error))[name_start:-2]
+    return name
