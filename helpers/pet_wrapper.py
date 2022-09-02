70f175000ab37e47d1ad7443c6d5d8852ddd1791
Athziri Rodríguez <arodriguez2@logitech.com>
2022-09-02 12:10:20 -0500

    Add basic tests for post, get, and delete

--- helpers/pet_wrapper.py
+++ helpers/pet_wrapper.py
@@ -1,3 +1,7 @@
+import json
+from json import JSONDecodeError
+from json import loads
+
 from base_wrapper import BaseWrapper
 from config import BASE_URI
 from core.api_core import APIRequest
@@ -15,4 +19,16 @@ class PetWrapper(BaseWrapper):
         if body is None:
             pet_id, payload = get_pet()
         response = self.request.post(self.base_url, payload, self.headers)
-        return pet_id, response
+        try:
+            payload = json.loads(payload)
+        except JSONDecodeError:
+            payload = {}
+        return pet_id, response, payload
+
+    def get_pet_by_id(self, pet_id):
+        url = f'{self.base_url}/{pet_id}'
+        return self.request.get(url)
+
+    def delete_pet_by_id(self, pet_id):
+        url = f'{self.base_url}/{pet_id}'
+        return self.request.delete(url)
--- tests/test_pet.py
+++ tests/test_pet.py
@@ -1,13 +1,39 @@
 import requests
 
 from helpers.pet_wrapper import PetWrapper
+from core.custom_assertions import assert_body_is_the_same
 from assertpy import assert_that
 
 wrapper = PetWrapper()
 
 
-def test_post_pet():
-    pet_id, response = wrapper.create_pet()
+def test_post_pet_op1():
+    pet_id, response, expected = wrapper.create_pet()
     assert_that(response.status_code).is_equal_to(requests.codes.ok)
-    assert_that(response.as_dict["id"]).is_equal_to(pet_id)
+    assert_body_is_the_same(expected, response.as_dict)
 
+
+def test_post_pet_op2():
+    pet_id, response, expected = wrapper.create_pet()
+    assert_that(response.status_code).is_equal_to(requests.codes.ok)
+    assert_body_is_the_same(expected, response.as_dict)
+
+    response_get = wrapper.get_pet_by_id(pet_id)
+    assert_body_is_the_same(response.as_dict, response_get.as_dict)
+
+
+def test_get_pet_by_id():
+    pet_id, response_pet, _ = wrapper.create_pet()
+    response = wrapper.get_pet_by_id(pet_id)
+    assert_that(response.status_code).is_equal_to(requests.codes.ok)
+    assert_body_is_the_same(response_pet.as_dict, response.as_dict)
+
+
+def test_delete_pet_by_id():
+    pet_id, response_pet, _ = wrapper.create_pet()
+    response = wrapper.delete_pet_by_id(pet_id)
+    assert_that(response.status_code).is_equal_to(requests.codes.ok)
+    assert_that(response.as_dict['message']).contains(str(pet_id))
+
+    response_get_deleted_pet = wrapper.get_pet_by_id(pet_id)
+    assert_that(response_get_deleted_pet.status_code).is_equal_to(requests.codes.not_found)
