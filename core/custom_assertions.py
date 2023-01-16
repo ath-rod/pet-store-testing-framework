61f0a7c8f4eb57016e07ed9ef2299fefacc8565b
Athziri Rodríguez <arodriguez2@logitech.com>
2023-01-16 13:41:37 -0600

    Recover accidentally removed modifications

---
+++ core/custom_assertions.py
@@ -0,0 +1,7 @@
+from assertpy import soft_assertions, assert_that
+
+
+def assert_body_is_the_same(expected, actual):
+    with soft_assertions():
+        for attribute in expected:
+            assert_that(expected[attribute]).described_as(f"{attribute}").is_equal_to(actual[attribute])
--- helpers/pet_wrapper.py
+++ helpers/pet_wrapper.py
@@ -1,3 +1,6 @@
+import json
+from json import JSONDecodeError
+
 from base_wrapper import BaseWrapper
 from config import BASE_URI
 from core.api_core import APIRequest
@@ -15,4 +18,16 @@ class PetWrapper(BaseWrapper):
         if body is None:
             pet_id, payload = get_pet()
         response = self.request.post(self.base_url, payload, self.headers)
-        return pet_id, response
+        try:
+            payload = json.loads(payload)
+        except JSONDecodeError:
+            payload = {}
+        return pet_id, response, payload
+
+    def get_pet_by_id(self, pet_id):
+        url = f'{self.base_url}/{pet_id}'
+        return self.request.get(url)
+
+    def delete_pet_by_id(self, pet_id):
+        url = f'{self.base_url}/{pet_id}'
+        return self.request.delete(url)
