ba7da868bbcff23663b008e6b5e43a2273003839
Athziri Rodríguez <arodriguez2@logitech.com>
2023-01-27 12:52:02 -0600

    Add request dataclass for pet wrapper and add modify pet test

--- core/api_core.py
+++ core/api_core.py
@@ -9,18 +9,22 @@ from utils.custom_strings import error_name
 @dataclass
 class Response:
     status_code: int
-    text: str  # TODO: investigate why renaming causes failure, rename to body_as_raw
+    body_as_raw: str
     body_as_dict: object
     headers: dict
 
 
 class APIRequest:
-    def get(self, url):
-        response = requests.get(url)
-        return self.__get_response_data(response)
-
     def post(self, url, payload, headers):
         response = requests.post(url, data=payload, headers=headers)
+        return self.__get_response_data(response)
+
+    def put(self, url, payload, headers):
+        response = requests.put(url, data=payload, headers=headers)
+        return self.__get_response_data(response)
+
+    def get(self, url):
+        response = requests.get(url)
         return self.__get_response_data(response)
 
     def delete(self, url):
@@ -35,7 +39,9 @@ class APIRequest:
 
         try:
             body_as_dict = response.json()
-        except JSONDecodeError as error:  # TODO: research how to clean this
+        except JSONDecodeError as error:
+            if len(body_as_raw) > 200:
+                body_as_raw = f"{body_as_raw[:150]} ... {body_as_raw[-40:]}"
             raise Exception(f"{error_name(error)} raised from status code {status_code}, headers: {headers},"
                             f"and body {body_as_raw}")
 
--- tests/test_pet.py
+++ tests/test_pet.py
@@ -1,33 +1,62 @@
-import requests
 from assertpy import assert_that
+from requests import codes
 
 from core.custom_assertions import assert_body_is_the_same
 from helpers.pet_wrapper import PetWrapper
+from resources import random_data_generator
+from utils.get_data_set import generate_pet
 
 wrapper = PetWrapper()
 
 
-def test_add_pet():  # TODO: explicit body (don't hide the test purpose)
-    pet_id, response, expected = wrapper.create_pet()
-    assert_that(response.status_code).is_equal_to(requests.codes.ok)
-    assert_body_is_the_same(expected, response.body_as_dict)
+def test_add_pet():
+    payload = {
+        "id": random_data_generator.get_random_number(),
+        "category": {
+            "id": random_data_generator.get_random_number(),
+            "name": random_data_generator.get_random_name()
+        },
+        "name": random_data_generator.get_random_name(),
+        "photoUrls": random_data_generator.get_random_list_of_names(),
+        "tags": [
+            {
+                "id": random_data_generator.get_random_number(),
+                "name": random_data_generator.get_random_name()
+            }
+        ],
+        "status": random_data_generator.get_random_element(["available", "pending", "sold"])
+    }
+    request = wrapper.post_pet(payload=payload)
+    assert_that(request.response.status_code, request.response.body_as_raw).is_equal_to(codes.ok)
+    assert_body_is_the_same(request.payload, request.response.body_as_dict)
 
-    response_get = wrapper.get_pet_by_id(pet_id)
-    assert_body_is_the_same(response.body_as_dict, response_get.body_as_dict)
+    response_get = wrapper.get_pet_by_id(request.pet_id)
+    assert_body_is_the_same(request.response.body_as_dict, response_get.body_as_dict)
 
 
 def test_get_pet():
-    pet_id, response_pet, _ = wrapper.create_pet()
-    response = wrapper.get_pet_by_id(pet_id)
-    assert_that(response.status_code).is_equal_to(requests.codes.ok)
-    assert_body_is_the_same(response_pet.body_as_dict, response.body_as_dict)
+    request_new_pet = wrapper.post_pet()
+    response = wrapper.get_pet_by_id(request_new_pet.pet_id)
+    assert_that(response.status_code, response.body_as_raw).is_equal_to(codes.ok)
+    assert_body_is_the_same(request_new_pet.response.body_as_dict, response.body_as_dict)
 
 
 def test_remove_pet():
-    pet_id, response_pet, _ = wrapper.create_pet()
-    response = wrapper.delete_pet_by_id(pet_id)
-    assert_that(response.status_code).is_equal_to(requests.codes.ok)
-    assert_that(response.body_as_dict['message']).contains(str(pet_id))
+    request_new_pet = wrapper.post_pet()
+    response = wrapper.delete_pet_by_id(request_new_pet.pet_id)
+    assert_that(response.status_code, response.body_as_raw).is_equal_to(codes.ok)
+    assert_that(response.body_as_dict['message']).contains(str(request_new_pet.pet_id))
 
-    response_get_deleted_pet = wrapper.get_pet_by_id(pet_id)
-    assert_that(response_get_deleted_pet.status_code).is_equal_to(requests.codes.not_found)
+    response_get_deleted_pet = wrapper.get_pet_by_id(request_new_pet.pet_id)
+    assert_that(response_get_deleted_pet.status_code).is_equal_to(codes.not_found)
+
+
+def test_modify_pet():  # TODO: keep investigating API as this test fails
+    request_new_pet = wrapper.post_pet()
+    new_pet_data = generate_pet(pet_id=request_new_pet.pet_id)  # TODO: should the explicit data be here?
+
+    response = wrapper.put_pet_by_id(request_new_pet.pet_id, new_pet_data)
+
+    assert_that(response.status_code, f"{response.body_as_raw} {response.headers}").is_equal_to(codes.ok)
+    assert_that(response.body_as_dict['message']).contains(str(request_new_pet.pet_id))
+    assert_body_is_the_same(response.body_as_dict, new_pet_data)
